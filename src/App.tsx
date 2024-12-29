dba5e2c8d36f822534fa5f748bf047539ee3de51
Kazarius40 <roman.dotsenko.de@gmail.com>
2024-12-29 08:22:33 +0100

    Тренуюся

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
+import MyComponent from "./components/MyComponent.tsx";
 
 function App() {
 
     return (
         <>
-
+            <MyComponent text={'Hi, I\'m Amazing!'}/>
+            <MyComponent text={'Hello World!'}/>
+            {/*{MyComponent({text: 'Hello World'})}*/}
         </>
     )
 }
