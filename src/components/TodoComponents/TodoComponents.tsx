6807b917135325162986e339a5ea7dfb7a99c039
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-01-07 04:40:56 +0100

    виконую друге завдання з другої домашки

--- src/components/TodoComponents/TodoComponents.tsx
+++ src/components/TodoComponents/TodoComponents.tsx
@@ -1,16 +1,17 @@
 import {TodoComponent} from "../TodoComponent/TodoComponent.tsx";
 import {useState} from "react";
+import {IToDo} from "../../models/ToDo/IToDo.ts";
 
-export const TodoComponents = ()=>{
-    const [users, setUsers] = useState([]);
+export const TodoComponents = () => {
+    const [users, setUsers] = useState<IToDo[]>([]);
     fetch('https://dummyjson.com/todos')
-    .then(res => res.json())
-    .then(json => {
-        setUsers(json);
-    });
+        .then(res => res.json())
+        .then(json => {
+            setUsers(json);
+        });
     return (
         {
-            users.map()
+            users.map((user) => <TodoComponent/>)
         }
     )
 }
