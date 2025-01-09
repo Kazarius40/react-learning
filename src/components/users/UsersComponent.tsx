099048147bd353b4d6fe0095ff4a3533d52e3295
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-01-09 06:25:52 +0100

    Початок виконання hw3/task1

--- src/components/users/UsersComponent.tsx
+++ src/components/users/UsersComponent.tsx
@@ -1,13 +1,19 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {IUser} from "../../models/IUser.ts";
 
 const UsersComponent = () => {
 
     const [users, setUsers] = useState<IUser[]>([]);
 
+    useEffect(() => {
+        getUsers().then(({users}) => setUsers(users));
+    }, [])
+
     return (
         <div>
-
+            {
+                users.map(user => <UserComponent item={user} />)
+            }
         </div>
     );
 };
