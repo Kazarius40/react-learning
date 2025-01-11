20c5a168d1ab662120fd6118f3b0a8ddb0f035a7
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-01-11 05:37:46 +0100

    Завершив виконання hw3/task1

--- src/services/api.services.ts
+++ src/services/api.services.ts
@@ -1,10 +1,10 @@
 import {urls} from "../constans/urls.ts";
-import {UsersResponse} from "../models/users/IUsersResponse.ts";
+import {IUsersResponse} from "../models/users/IUsersResponse.ts";
 import {IPostsResponse} from "../models/posts/IPostsResponse.ts";
 
 
 export const userService = {
-    getUsers: async (): Promise<UsersResponse> => {
+    getUsers: async (): Promise<IUsersResponse> => {
         return await fetch(urls.users.base)
             .then(res => res.json())
     }
