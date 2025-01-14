369fa334098c5ff0a07deb61b8a3e6bf981bce81
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-01-14 05:02:53 +0100

    zdf

--- src/components/carts-component/CartsComponent.tsx
+++ src/components/carts-component/CartsComponent.tsx
@@ -1,5 +1,11 @@
-export const CartsComponent = () => {
+import {useEffect} from "react";
 
+type idProps = {
+    id: string;
+}
+
+export const CartsComponent = ({id}: idProps) => {
+useEffect()
 
     return (
         <>
