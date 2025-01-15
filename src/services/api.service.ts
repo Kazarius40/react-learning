23b537eae22b45fb3a6f70aaf6bb4d3d2e812208
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-01-15 05:42:35 +0100

    Прибрав зайву інтерполяцію

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -2,7 +2,7 @@ const baseUrl = import.meta.env.VITE_BASE_URL;
 console.log(baseUrl);
 
 export const getAll = async <T, >(endpoint: string): Promise<T> => {
-    return await fetch(`${baseUrl}${endpoint}`).then((res)  => res.json() as T);
+    return await fetch(baseUrl + endpoint).then((res)  => res.json() as T);
 }
 
 // export const cartsServices = {
