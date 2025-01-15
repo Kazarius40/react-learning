c8eed3c49610b2e2d8f199cc9c5b7e08a278c009
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-01-15 05:22:01 +0100

    Переробив, щоб викликати все однією функцією

--- src/components/carts-component/CartsComponent.tsx
+++ src/components/carts-component/CartsComponent.tsx
@@ -1,7 +1,8 @@
 import {useEffect, useState} from "react";
-import {cartsServices} from "../../services/api.service.ts";
+import {getAll} from "../../services/api.service.ts";
 import {ICarts} from "../../models/carts-models/ICarts.ts";
 import {CartComponent} from "./CartComponent.tsx";
+import {IBaseResponseModel} from "../../models/IBaseResponseModel.ts";
 
 type idProps = {
     id: string;
@@ -11,7 +12,7 @@ export const CartsComponent = ({ id }: idProps) => {
     const [carts, setCarts] = useState<ICarts[]>([]);
 
     useEffect(() => {
-        cartsServices.getAllCartsOfUserById(id).then(({ carts }) =>
+        getAll<IBaseResponseModel & {carts: ICarts[]}>(`users/${id}/carts`).then(({ carts }) =>
             setCarts(carts)
         );
     }, [id]);
--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,14 +1,11 @@
-import {ICarts} from "../models/carts-models/ICarts.ts";
-import {IBaseResponseModel} from "../models/IBaseResponseModel.ts";
-
 const baseUrl = import.meta.env.VITE_BASE_URL;
 console.log(baseUrl);
 
 export const getAll = async <T, >(endpoint: string): Promise<T> => {
-    return await fetch(`${baseUrl + '/'}${endpoint}`).then((res) => res.json());
+    return await fetch(`${baseUrl}/${endpoint}`).then((res) => res.json());
 }
 
-export const cartsServices = {
-    getAllCartsOfUserById: async (userId: string): Promise<IBaseResponseModel & {carts: ICarts[]}> => {
-        return await fetch(baseUrl + '/users/' + userId +'/carts').then((res) => res.json())}
-}
+// export const cartsServices = {
+//     getAllCartsOfUserById: async (userId: string): Promise<IBaseResponseModel & {carts: ICarts[]}> => {
+//         return await fetch(baseUrl + '/users/' + userId +'/carts').then((res) => res.json())}
+// }
