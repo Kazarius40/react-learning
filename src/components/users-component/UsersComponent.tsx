cfb739149b14c0477421a850b8ca44cc1d5965cb
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-01-15 06:01:25 +0100

    Додав деякі файли

--- src/components/users-component/UsersComponent.tsx
+++ src/components/users-component/UsersComponent.tsx
@@ -1,15 +1,21 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {getAll} from "../../services/api.services.ts";
+import {IBaseResponse} from "../../models/IBaseResponse.ts";
+import {IUser} from "../../models/users/IUser.ts";
 
 export const UsersComponent = () => {
 
+    const [users, setUsers] = useState();
+
     useEffect(() => {
-        getAll('users').then()
+        getAll<IBaseResponse & {users: IUser[]}>('users').then(({users}) => setUsers(users));
     }, []);
     
     return (
         <>
-
+            {
+                users.map((user) => {})
+            }
         </>
     );
 };
