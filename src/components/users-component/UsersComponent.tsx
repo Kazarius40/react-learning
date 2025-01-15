73ff56815a809b0a7d00ea879df688c6b608f8eb
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-01-15 05:52:00 +0100

    Додав деяку типізацію

--- src/components/users-component/UsersComponent.tsx
+++ src/components/users-component/UsersComponent.tsx
@@ -1,4 +1,12 @@
+import {useEffect} from "react";
+import {getAll} from "../../services/api.services.ts";
+
 export const UsersComponent = () => {
+
+    useEffect(() => {
+        getAll('users').then()
+    }, []);
+    
     return (
         <>
 
