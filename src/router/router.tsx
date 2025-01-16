405a7d08d8c6db992ae691520c8b72ef1c23030e
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-01-16 06:26:45 +0100

    Додав деякі файли

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,10 +2,17 @@ import {createBrowserRouter} from "react-router-dom";
 import {MainLayout} from "../layouts/MainLayout.tsx";
 import {UsersPage} from "../pages/UsersPage.tsx";
 import {UsersDetailsPage} from "../pages/UsersDetailsPage.tsx";
+import {PaginationLayout} from "../layouts/PaginationLayout.tsx";
 
 export const routes = createBrowserRouter([
-    {path: '', element: <MainLayout/>, children: [
-            {path: 'users', element: <UsersPage/>},
-            {path: 'users/details', element: <UsersDetailsPage/>},
-        ]}
+    {
+        path: '', element: <MainLayout/>, children: [
+            {
+                path: 'list', element: <PaginationLayout/>, children: [
+                    {path: 'users', element: <UsersPage/>},
+                    {path: 'users/details', element: <UsersDetailsPage/>},
+                ]
+            }
+        ]
+    }
 ]);
