e6c8bf965428cb19614a36730cc2391f0521a43a
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-01-17 05:14:04 +0100

    Розпочав повторне тренування в написанні hw4

--- src/components/users/UsersComponent.tsx
+++ src/components/users/UsersComponent.tsx
@@ -1,4 +1,14 @@
+import {useEffect} from "react";
+import {getAll} from "../../services/api.services.ts";
+import {IBaseResponse} from "../../models/IBaseResponse.ts";
+import {IUser} from "../../models/users/IUser.ts";
+
 export const UsersComponent = () => {
+
+    useEffect(() => {
+        getAll<IBaseResponse & {users: IUser[]}>('/users').then({users} => )
+    }, []);
+
     return (
         <>
         </>
