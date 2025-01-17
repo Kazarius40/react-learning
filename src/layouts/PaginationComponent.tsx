a1f2928ec27520dd2277d7ba3cb5eb1000d0e45f
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-01-17 06:13:12 +0100

    Розпочав повторне тренування в написанні hw4

--- src/layouts/PaginationComponent.tsx
+++ src/layouts/PaginationComponent.tsx
@@ -1,14 +1,13 @@
 import {useSearchParams} from "react-router-dom";
 
 export const PaginationComponent = () => {
-    const [searchParams, setSearchParams] = useSearchParams({page: '1'});
-    const page = Number(searchParams.get('page')) || 1;
+    const [searchParams, setSearchParams] = useSearchParams();
+    let currentPage = Number(searchParams.get('page')) || 1;
 
     return (
         <>
             <button onClick={() => {
-                let currentPage = page;
-                setSearchParams((--currentPage).toString());
+                setSearchParams({page: (--currentPage).toString()});
             }}>prev
             </button>
             <button>next</button>
