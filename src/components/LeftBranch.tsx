dca3898ba257be9bbcd84d736f24beb13300ffa6
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-01-26 05:59:16 +0100

    Почав проходити 7-у лекцію з context

---
+++ src/components/LeftBranch.tsx
@@ -0,0 +1,11 @@
+import {LeftBranchA} from "./LeftBranchA.tsx";
+
+export const LeftBranch = () => {
+    return (
+        <div>LeftBranch
+            <p>
+                <LeftBranchA/>
+            </p>
+        </div>
+    );
+};
---
+++ src/components/RightBranch.tsx
@@ -0,0 +1,11 @@
+import {RightBranchA} from "./RightBranchA.tsx";
+
+export const RightBranch = () => {
+    return (
+        <div>RightBranch
+            <p>
+                <RightBranchA/>
+            </p>
+        </div>
+    );
+};
