062dfe055e568a66ef512a1521d05fe9eebfb10b
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-01-29 04:53:24 +0100

    Заверши опрацювання лекції 8-1/redux

--- src/components/userscomponent/UsersComponent.tsx
+++ src/components/userscomponent/UsersComponent.tsx
@@ -1,7 +1,31 @@
+import {useAppSelector, userSliceActions} from "../../main.tsx";
+import {useDispatch} from "react-redux";
+import {useEffect} from "react";
+
 export const UsersComponent = () => {
+
+    const {users} = useAppSelector(({userSlice}) => userSlice);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(value => value.json())
+            .then(value => {
+                dispatch(userSliceActions.loadUsers(value));
+            });
+
+    }, []);
+
+
     return (
         <div>
-            UsersComponent
+            {
+                users.map((user) => {
+                    return <div key={user.id}>
+                        {user.name}
+                    </div>
+                })
+            }
         </div>
     );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,41 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import {RouterProvider} from "react-router-dom";
 import {routes} from "./router/router.tsx";
+import {Provider, useSelector} from "react-redux";
+import {configureStore, createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {IUser} from "./models/user/IUser.ts";
 
-createRoot(document.getElementById('root')!).render(<RouterProvider router={routes}/>)
+type UserSliceType = {
+    users: IUser[];
+}
+
+const initialState: UserSliceType = {users: []};
+
+export const userSlice = createSlice({
+    name: "userSlice",
+    initialState: initialState,
+    reducers: {
+        loadUsers: (state, action: PayloadAction<IUser[]>) => {
+            state.users = action.payload;
+        }
+    }
+});
+
+export const userSliceActions = {
+    ...userSlice.actions,
+}
+
+const store = configureStore({
+    reducer: {
+        userSlice: userSlice.reducer,
+        // postSlice: null
+    }
+});
+
+export const useAppSelector = useSelector.withTypes<ReturnType<typeof store.getState>>();
+
+createRoot(document.getElementById('root')!).render(
+    <Provider store={store}>
+        <RouterProvider router={routes}/>
+    </Provider>
+);
