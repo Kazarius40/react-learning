ce4068b704d1fde21599872593617af0d84568a2
Kazarius40 <roman.dotsenko.de@gmail.com>
2025-01-30 11:59:20 +0100

    Оптимізую код, щоб розібратися, що, як і чому працює

--- src/redux/slices/userSlice/userApi.ts
+++ src/redux/slices/userSlice/userApi.ts
@@ -1,8 +1,6 @@
 const baseUrl = import.meta.env.VITE_BASE_URL;
 
-const fetchData = async (endpoint: string) => {
-    const response = await fetch(baseUrl + endpoint);
-    return response.json();
-};
+const fetchData = async (endpoint: string) =>
+    (await fetch(baseUrl + endpoint)).json()
 
 export const fetchUser = (id?: string) => fetchData(id ? '/users/' + id : '/users');
